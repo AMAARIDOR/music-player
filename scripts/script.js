6ce1d265c9ca02f4b29c5c63dc231948363c9940
HZZRD <arish2k3@gmail.com>
2023-01-28 16:35:04 +0530

    select elements js

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// Elements:
+
+const track_status = document.querySelector(".song-status");
+const track_cover = document.querySelector(".song-image");
+const track_name = document.querySelector(".song-name");
+const track_artist = document.querySelector(".song-artist");
+
+const current_time = document.querySelector(".current-time");
+const total_duration = document.querySelector(".total-duration");
+
+const seek_slider = document.querySelector(".audio-slider");
+const volume_slider = document.querySelector(".volume-slider");
+
+const previous_button = document.querySelector(".previous-button");
+const play_pause_button = document.querySelector(".play-pause-button");
+const next_button = document.querySelector(".next-button");
