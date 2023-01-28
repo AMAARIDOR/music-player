32f544594e6c24a87e57141d2da1d553bc3f064c
HZZRD <arish2k3@gmail.com>
2023-01-28 20:28:48 +0530

    fix play index 2 music bug

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,6 +5,9 @@ let is_playing = false;
 
 function updateTrack() {
   audio_element.src = songs_list[track_index].path;
+  track_status.textContent = `Now playing audio ${track_index + 1} of ${
+    songs_list.length
+  }`;
   track_cover.src = songs_list[track_index].image;
   track_name.textContent = songs_list[track_index].name;
   track_artist.textContent = songs_list[track_index].artist;
@@ -19,17 +22,17 @@ function pauseTrack() {
 }
 
 function nextTrack() {
-  track_index += 1;
   if (track_index === songs_list.length) track_index = 0;
   updateTrack();
   loadTrack();
   is_playing = true;
+  track_index += 1;
 }
 
 function previousTrack() {
-  track_index -= 1;
   if (track_index < 0) track_index = songs_list.length - 1;
   updateTrack();
   loadTrack();
   is_playing = true;
+  track_index -= 1;
 }
