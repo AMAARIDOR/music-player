4e66033d3d367bf10e5c00780d6b6648569ae624
HZZRD <arish2k3@gmail.com>
2023-01-28 18:07:15 +0530

    add next track and loop tracks functionality but not optimized

--- scripts/script.js
+++ scripts/script.js
@@ -5,25 +5,37 @@ let is_playing = false;
 
 // Functions:
 
+function changeTrack() {
+  audio_element.src = songs_list[track_index].path;
+}
+
+function loopTracks() {
+  if (track_index === songs_list.length) {
+    track_index = 0;
+  } else if (track_index < 0) {
+    track_index = songs_list.length - 1;
+  }
+}
+
 function nextTrack() {
-  if (track_index >= 0) track_index += 1;
-  // console.log(track_index);
+  track_index += 1;
+  loopTracks();
+  changeTrack();
+  console.log(audio_element.src);
 }
 
 function previousTrack() {
-  if (track_index > 0) track_index -= 1;
-  // console.log(track_index);
+  track_index -= 1;
+  loopTracks();
+  changeTrack();
+  console.log(audio_element.src);
 }
 
 // Event Handlers:
 
-next_button.addEventListener("click", () => {
-  nextTrack();
-});
+next_button.addEventListener("click", nextTrack);
 
-previous_button.addEventListener("click", () => {
-  previousTrack();
-});
+previous_button.addEventListener("click", previousTrack);
 
 play_pause_button.addEventListener("click", () => {
   if (!is_playing) {
