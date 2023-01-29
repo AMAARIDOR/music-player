ae703995a539ec1dd82f9a204444680ea2bfe14f
HZZRD <arish2k3@gmail.com>
2023-01-29 18:50:21 +0530

    restart project

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const trackStatus = document.querySelector(".song-status");
-const trackCoverContainer = document.querySelector(".song-image-container");
 const trackCover = document.querySelector(".song-image");
 const trackName = document.querySelector(".song-name");
 const trackArtist = document.querySelector(".song-artist");
@@ -16,61 +15,61 @@ const previousButton = document.querySelector(".previous-button");
 const playPauseButton = document.querySelector(".play-pause-button");
 const nextButton = document.querySelector(".next-button");
 
-const audioElement = document.querySelector(".audio-element");
+const audioElement = document.createElement("audio");
 
 let trackIndex = 0;
 let isPlaying = false;
 
-function updateTrack() {
-  audioElement.src = songsList[trackIndex].path;
-  trackCover.src = songsList[trackIndex].image;
-  trackName.textContent = songsList[trackIndex].name;
-  trackArtist.textContent = songsList[trackIndex].artist;
-  trackStatus.textContent = `Now playing audio ${trackIndex + 1} of ${
-    songsList.length
-  }`;
+// function updateTrack() {
+//   audioElement.src = songsList[trackIndex].path;
+//   trackCover.src = songsList[trackIndex].image;
+//   trackName.textContent = songsList[trackIndex].name;
+//   trackArtist.textContent = songsList[trackIndex].artist;
+//   trackStatus.textContent = `Now playing audio ${trackIndex + 1} of ${
+//     songsList.length
+//   }`;
 
-  audioElement.addEventListener("canplaythrough", () => {
-    totalDuration.textContent = audioElement.duration;
-  });
-}
+//   audioElement.addEventListener("canplaythrough", () => {
+//     totalDuration.textContent = audioElement.duration;
+//   });
+// }
 
-function loadTrack() {
-  audioElement.play();
-}
+// function loadTrack() {
+//   audioElement.play();
+// }
 
-function pauseTrack() {
-  audioElement.pause();
-}
+// function pauseTrack() {
+//   audioElement.pause();
+// }
 
-function nextTrack() {
-  if (trackIndex === songsList.length) trackIndex = 0;
-  updateTrack();
-  loadTrack();
-  isPlaying = true;
-  trackIndex += 1;
-  console.log(trackIndex);
-}
+// function nextTrack() {
+//   if (trackIndex === songsList.length) trackIndex = 0;
+//   updateTrack();
+//   loadTrack();
+//   isPlaying = true;
+//   trackIndex += 1;
+//   console.log(trackIndex);
+// }
 
-function previousTrack() {
-  if (trackIndex < 0) trackIndex = songsList.length - 1;
-  updateTrack();
-  loadTrack();
-  isPlaying = true;
-  trackIndex -= 1;
-  console.log(trackIndex);
-}
+// function previousTrack() {
+//   if (trackIndex < 0) trackIndex = songsList.length - 1;
+//   updateTrack();
+//   loadTrack();
+//   isPlaying = true;
+//   trackIndex -= 1;
+//   console.log(trackIndex);
+// }
 
-nextButton.addEventListener("click", nextTrack);
+// nextButton.addEventListener("click", nextTrack);
 
-previousButton.addEventListener("click", previousTrack);
+// previousButton.addEventListener("click", previousTrack);
 
-playPauseButton.addEventListener("click", () => {
-  if (!isPlaying) {
-    isPlaying = true;
-    loadTrack();
-  } else {
-    isPlaying = false;
-    pauseTrack();
-  }
-});
+// playPauseButton.addEventListener("click", () => {
+//   if (!isPlaying) {
+//     isPlaying = true;
+//     loadTrack();
+//   } else {
+//     isPlaying = false;
+//     pauseTrack();
+//   }
+// });
