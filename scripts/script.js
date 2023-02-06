f32c5cf48088223517930c51677543de47d070cd
HZZRD <arish2k3@gmail.com>
2023-02-06 12:22:52 +0530

    v 2.01.1: added eslint for refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 "use strict";
 
 const trackStatus = document.querySelector(".song-status");
@@ -16,6 +17,39 @@ const playPauseButton = document.querySelector(".play-pause-button");
 const nextButton = document.querySelector(".next-button");
 
 const audioElement = document.createElement("audio");
+
+const songsList = [
+  {
+    name: "On tha Linë",
+    artist: "Yeat",
+    image: "/assets/images/On-tha-line.jpg",
+    path: "/assets/music/On tha linë.mp3",
+  },
+  {
+    name: "How Long",
+    artist: "Charlie Puth",
+    image: "/assets/images/How-Long.png",
+    path: "/assets/music/How Long.mp3",
+  },
+  {
+    name: "Vamp Anthem",
+    artist: "Playboi Carti",
+    image: "/assets/images/Vamp-Anthem.jpeg",
+    path: "/assets/music/Vamp Anthem.mp3",
+  },
+  {
+    name: "Emotionally Scarred",
+    artist: "Lil Baby",
+    image: "/assets/images/Emotionally-Scarred.jpeg",
+    path: "/assets/music/Emotionally Scarred.mp3",
+  },
+  {
+    name: "New Rules",
+    artist: "Dua Lipa",
+    image: "/assets/images/New-Rules.jpeg",
+    path: "/assets/music/New Rules.mp3",
+  },
+];
 
 let trackIndex = -1;
 let isPlaying = false;
